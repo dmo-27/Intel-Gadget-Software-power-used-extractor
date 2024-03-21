f12ccae4e2b64ab62ff20e27658bb2cd92d71be5
Dhanashree Otari <113616820+dmo-27@users.noreply.github.com>
2024-03-21 15:12:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,13 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-
+app.use(
+  cors(
+    origin:["https://deploy-mern-intelgidea.vercel.app"];
+    methods:["POST","GET"];
+    credentials:true
+  )
+);
 
 app.use('/uploadcsv', require('./routes/csvRouter'));
 app.use('/acc',require('./routes/accRouter'));
